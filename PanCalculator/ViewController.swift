6bcb426ea590156bc26d0bf011e3405f22ac7496
Philipp Pan <philka9699@gmail.com>
2020-12-24 15:05:50 +0300

    Input Function

--- PanCalculator/ViewController.swift
+++ PanCalculator/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func inputOperand(_ sender: UIButton) {
+        if operand.isEmpty {
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        } else {
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
     }
-
-
+    @IBAction func clearAction(_ sender: UIButton) {
+    }
+    @IBAction func inputNumber(_ sender: UIButton) {
+    }
 }
 
