3c5fb446d812f0af030649b35fc32b49c3d0d7c2
Philipp Pan <philka9699@gmail.com>
2020-12-24 14:24:57 +0300

    Initial Commit

---
+++ PanCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanCalculator
+//
+//  Created by user on 24/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
